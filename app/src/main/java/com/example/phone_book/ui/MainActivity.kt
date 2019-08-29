ffe925a886c16302c4a39d075bdbc02b73c034a8
spacelinkers <josen221@gmail.com>
2019-08-30 00:33:26 +0600

    refactoring main activity code.

--- app/src/main/java/com/example/phone_book/ui/MainActivity.kt
+++ app/src/main/java/com/example/phone_book/ui/MainActivity.kt
@@ -5,24 +5,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.example.phone_book.ui.contactList.ContactFragment
-import com.example.phone_book.ui.favouriteList.FavouriteFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navController: NavController = findNavController(R.id.fragment_container)
-        bottomNavigationView.setupWithNavController(navController)
+        setupNavigation()
 
+    }
+
+    private fun setupNavigation(){
+        // BottomNavigationView
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
+        // NavController
+        val navController: NavController = findNavController(R.id.fragment_container)
+        // Integrating BottomNavigationView and NavController
+        bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp() =
