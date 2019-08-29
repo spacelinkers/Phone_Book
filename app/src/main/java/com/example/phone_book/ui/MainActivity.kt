dd4a6d2d1b62679ce095bf4ce9eb7e8d992ddf46
spacelinkers <josen221@gmail.com>
2019-08-30 00:26:18 +0600

    Integrating BottomNavigationView with NavController

--- app/src/main/java/com/example/phone_book/ui/MainActivity.kt
+++ app/src/main/java/com/example/phone_book/ui/MainActivity.kt
@@ -15,46 +15,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_contact -> {
-                val fragment = ContactFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_favourite -> {
-                val fragment = FavouriteFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController: NavController = findNavController(R.id.fragment_container)
+        bottomNavigationView.setupWithNavController(navController)
 
-
-//        val fragment = ContactFragment()
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
-//            .commit()
-
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-
-        navView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.fragment_container).navigateUp()
+
 }
