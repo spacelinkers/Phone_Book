13b91aafdc7a956a8c06beb22858020683538724
spacelinkers <josen221@gmail.com>
2019-08-31 00:45:53 +0600

    navigation bar selective show.

--- app/src/main/java/com/example/phone_book/ui/MainActivity.kt
+++ app/src/main/java/com/example/phone_book/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.phone_book
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -8,6 +9,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +28,18 @@ class MainActivity : AppCompatActivity() {
         val navController: NavController = findNavController(R.id.fragment_container)
         // Integrating BottomNavigationView and NavController
         bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{ _, destination, _ ->
+            if(destination.id == R.id.detailsFragment){
+                bottom_bar.visibility = View.GONE
+                fab.visibility = View.GONE
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                bottom_bar.visibility = View.VISIBLE
+                fab.visibility = View.VISIBLE
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onSupportNavigateUp() =
