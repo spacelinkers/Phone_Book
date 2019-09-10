7dfb9310e6d76ace08030c138dc9c0e9c72b7070
spacelinkers <josen221@gmail.com>
2019-09-10 22:24:40 +0600

    Adjusting UI for add to favourite.

--- app/src/main/java/com/example/phone_book/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/phone_book/ui/details/DetailsFragment.kt
@@ -1,13 +1,16 @@
 package com.example.phone_book.ui.details
 
 import android.content.Intent
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
@@ -33,10 +36,12 @@ class DetailsFragment : Fragment() {
 
         val contactId = arguments!!.getInt(getString(R.string.CONTACT_ID))
         Log.d("AAA", ""+ contactId.toString())
+
         contactId.let {
             detailsViewModel.getContactDetails(contactId).observe(this, Observer { contactDetails ->
                 populateContactDetails(contactDetails)
                 dialInit(contactDetails)
+                addToFavourite(contactDetails, view)
             })
         }
 
@@ -46,6 +51,7 @@ class DetailsFragment : Fragment() {
             findNavController().navigate(R.id.action_detailsFragment_to_editFragment, infoBundle)
         }
     }
+
 
     private fun populateContactDetails(contact: Contact?){
         details_name.text = contact?.firstName + " " + contact?.lastName
@@ -57,6 +63,27 @@ class DetailsFragment : Fragment() {
             val dialIntent = Intent(Intent.ACTION_DIAL)
             dialIntent.data = Uri.parse("tel:${contact.phone}")
             context?.startActivity(dialIntent)
+        }
+    }
+
+    private fun addToFavourite(contact: Contact, view: View){
+        val favouriteButton: ImageButton = view.findViewById(R.id.add_favourtie_image_button)
+        if(contact.favouriteFlag == 0){
+            Log.d("AAA", "Click A")
+            favouriteButton.setBackgroundResource(R.drawable.ic_favourite_outilne)
+        } else{
+            Log.d("AAA", "Click B")
+            favouriteButton.setBackgroundResource(R.drawable.ic_favourite)
+        }
+
+        favouriteButton.setOnClickListener{
+            if(contact.favouriteFlag == 0){
+                Log.d("AAA", "Click C")
+                favouriteButton.setBackgroundResource(R.drawable.ic_favourite)
+            } else{
+                Log.d("AAA", "Click D")
+                favouriteButton.setBackgroundResource(R.drawable.ic_favourite_outilne)
+            }
         }
     }
 }
